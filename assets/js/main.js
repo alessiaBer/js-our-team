a1e184db3fc5a6a05e1160adbbf4be88df346a95
alessiaBer <alexdeyn@gmail.com>
2023-03-22 14:25:32 +0100

    [add] array of objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,38 @@ Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
 Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg ***/
+
+
+//creo l'array di oggetti rappresentanti i membri del team
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
